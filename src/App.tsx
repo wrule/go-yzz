288601d407d071ab721f64eecb3a798169180b5d
jimao <shihao.gu@perfma.com>
2024-10-12 13:28:33 +0800

    1012

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,17 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const month = 1;
+
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        <a href=" " target="_blank">
+          < img src={viteLogo} className="logo" alt="Vite logo" />
+        </a >
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          < img src={reactLogo} className="logo react" alt="React logo" />
+        </a >
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -22,14 +24,70 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+          Edit <code>src/App.tsx</code> and save to test HMR1 <br />
+          1月是{1 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(1)}<br />
+          2月是{2 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(2)}<br />
+          3月是{3 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(3)}<br />
+          4月是{4 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(4)}<br />
+          5月是{5 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(5)}<br />
+          6月是{6 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(6)}<br />
+          7月是{7 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(7)}<br />
+          8月是{8 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(8)}<br />
+          9月是{9 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(9)}<br />
+          10月是{10 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(10)}<br />
+          11月是{11 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(11)}<br />
+          12月是{12 > 6 ? '下半年' : '上半年'}，英文是{month_num_to_english(12)}<br />
+        </p >
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p >
     </>
   )
 }
+function month_num_to_english(month:number){
+  
+  if (month==1) {
+    return 'January';
+  }
+  if (month==2) {
+    return 'February';
+  }
+  if (month==3) {
+    return 'March';
+  }
+  if (month==4) {
+    return 'April';
+  }
+  if (month==5) {
+    return 'May';
+  }
+  if (month==6) {
+    return 'June';
+  }
+  if (month==7) {
+    return 'July';
+  }
+  if (month==8) {
+    return 'August';
+  }
+  if (month==9) {
+    return 'September';
+  }
+  if (month==10) {
+    return 'October';
+  }
+  if (month==11) {
+    return 'November';
+  }
+  if (month==12) {
+    return 'December';
+  }
+  else{
+    return '啥也不是';
+  }
+
+}
+console.log(month_num_to_english)
 
 export default App
