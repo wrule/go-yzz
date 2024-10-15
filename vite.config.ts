4a69582d267d9dfc6efdd95b50f62910893bcb6f
jimao <shihao.gu@perfma.com>
2024-10-15 12:38:37 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     open: true,
-    host: '127.0.0.1',
+    host: '0.0.0.0',
+    port: 9873,
   },
   plugins: [react()],
 })
