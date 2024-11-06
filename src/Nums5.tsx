3b6ec294227110da96a06a7f30c9355df6118128
yanzhenzhen.88 <yanzhenzhen.88@bytedance.com>
2024-11-06 12:50:27 +0800

    质数验证

--- src/Nums5.tsx
+++ src/Nums5.tsx
@@ -3,10 +3,27 @@
 // 挑战，如果某一个数不是质数的话，显示原因，如果是质数的话不需要显示原因
 
 export default
-function Nums() {
-  const num = 4;
-
+  function Nums() {
+  const num = 10;
   return <div style={{ fontSize: '32px' }}>
-    <span>{num} 不是质数 原因是 它能被 2 整除</span>
+    <span>{num}{Verify(num)} </span>
   </div>;
-}
+
+  function Verify(num: number) {
+    if (num > 1) {
+      for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+          return <span>不是质数, 原因是它能被{i}整除</span>
+        }
+      }
+      return '是质数'
+
+    }
+    else {
+      return '不是质数, 原因是小于等于1'
+    }
+
+
+  }
+
+  }
